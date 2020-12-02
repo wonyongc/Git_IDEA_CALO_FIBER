a9d4a0a4bb4303650e66801ddf0a6544d7265fce
Marco Toliman Lucchini <marco.toliman.lucchini@cern.ch>
2020-12-02 16:33:20 +0100

    setting output filename from outside

--- DRC4pifibers/DRC4pigeometry/Dream/B4/B4a/src/B4RunAction.cc
+++ DRC4pifibers/DRC4pigeometry/Dream/B4/B4a/src/B4RunAction.cc
@@ -126,8 +126,8 @@ void B4RunAction::BeginOfRunAction(const G4Run* run)
   
 //   Open an output file    
 //   G4String fileName = "B4.root";    
-//   G4String fileName = outputFileName;    
-  G4String fileName = "out.root";    
+  G4String fileName = outputFileName;    
+//   G4String fileName = "out.root";    
   analysisManager->OpenFile(fileName);    
   
   analysisManager->SetVerboseLevel(0);
