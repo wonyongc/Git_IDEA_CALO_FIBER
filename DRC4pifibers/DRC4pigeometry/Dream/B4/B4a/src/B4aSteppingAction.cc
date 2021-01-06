791b3568618eefbd869fd4994377868795e551fe
lopezzot <lorenzo.pezzotti01@gmail.com>
2021-01-06 13:57:29 +0100

    fix_hit_bug

--- DRC4pifibers/DRC4pigeometry/Dream/B4/B4a/src/B4aSteppingAction.cc
+++ DRC4pifibers/DRC4pigeometry/Dream/B4/B4a/src/B4aSteppingAction.cc
@@ -223,9 +223,9 @@ void B4aSteppingAction::UserSteppingAction(const G4Step* step)
 		if (s_signal>0.0){
 		  fEventAction->WriteFiber_Info(S_fiber_ID,s_signal,1,vectPostip,copynumberslice,copynumbertower);// 1 == S 0 == C
 		// Extract info for z time
-		std::ofstream TimeFile;
-		TimeFile.open("Time.txt", std::ios_base::app);
-		TimeFile<<"Scin "<< std::fixed << std::setprecision(3) <<S_fiber_ID<<" "<<vectPostip.getX()<<" "<<vectPostip.getY()<<" "<<vectPostip.getZ()<<" "<<s_signal<<" "<<sqrt((SiPMvecPos[0]-step->GetTrack()->GetPosition().getX())*(SiPMvecPos[0]-step->GetTrack()->GetPosition().getX())+(SiPMvecPos[1]-step->GetTrack()->GetPosition().getY())*(SiPMvecPos[1]-step->GetTrack()->GetPosition().getY())+(SiPMvecPos[2]-step->GetTrack()->GetPosition().getZ())*(SiPMvecPos[2]-step->GetTrack()->GetPosition().getZ()))<<" "<<step->GetTrack()->GetGlobalTime()<<G4endl;
+		//std::ofstream TimeFile;
+		//TimeFile.open("Time.txt", std::ios_base::app);
+		//TimeFile<<"Scin "<< std::fixed << std::setprecision(3) <<S_fiber_ID<<" "<<vectPostip.getX()<<" "<<vectPostip.getY()<<" "<<vectPostip.getZ()<<" "<<s_signal<<" "<<sqrt((SiPMvecPos[0]-step->GetTrack()->GetPosition().getX())*(SiPMvecPos[0]-step->GetTrack()->GetPosition().getX())+(SiPMvecPos[1]-step->GetTrack()->GetPosition().getY())*(SiPMvecPos[1]-step->GetTrack()->GetPosition().getY())+(SiPMvecPos[2]-step->GetTrack()->GetPosition().getZ())*(SiPMvecPos[2]-step->GetTrack()->GetPosition().getZ()))<<" "<<step->GetTrack()->GetGlobalTime()<<G4endl;
 		//TimeFile.close();
                     }
 	}
@@ -313,9 +313,9 @@ void B4aSteppingAction::UserSteppingAction(const G4Step* step)
 	      if (c_signal>0){
 				  fEventAction->WriteFiber_Info(C_fiber_ID,c_signal,0,vectPostip,copynumberslice,copynumbertower);// 1 == S 0 == C
 				  // Extract info for z time
-				  std::ofstream TimeFile;
-				  TimeFile.open("Time.txt", std::ios_base::app);
-	  		  TimeFile<<"Cher "<<std::fixed << std::setprecision(3) <<C_fiber_ID<<" "<<vectPostip.getX()<<" "<<vectPostip.getY()<<" "<<vectPostip.getZ()<<" "<<c_signal<<" "<<sqrt((SiPMvecPos[0]-step->GetTrack()->GetPosition().getX())*(SiPMvecPos[0]-step->GetTrack()->GetPosition().getX())+(SiPMvecPos[1]-step->GetTrack()->GetPosition().getY())*(SiPMvecPos[1]-step->GetTrack()->GetPosition().getY())+(SiPMvecPos[2]-step->GetTrack()->GetPosition().getZ())*(SiPMvecPos[2]-step->GetTrack()->GetPosition().getZ()))<<" "<<step->GetTrack()->GetGlobalTime()<<G4endl;
+				  //std::ofstream TimeFile;
+				  //TimeFile.open("Time.txt", std::ios_base::app);
+	  		  //TimeFile<<"Cher "<<std::fixed << std::setprecision(3) <<C_fiber_ID<<" "<<vectPostip.getX()<<" "<<vectPostip.getY()<<" "<<vectPostip.getZ()<<" "<<c_signal<<" "<<sqrt((SiPMvecPos[0]-step->GetTrack()->GetPosition().getX())*(SiPMvecPos[0]-step->GetTrack()->GetPosition().getX())+(SiPMvecPos[1]-step->GetTrack()->GetPosition().getY())*(SiPMvecPos[1]-step->GetTrack()->GetPosition().getY())+(SiPMvecPos[2]-step->GetTrack()->GetPosition().getZ())*(SiPMvecPos[2]-step->GetTrack()->GetPosition().getZ()))<<" "<<step->GetTrack()->GetGlobalTime()<<G4endl;
 				//TimeFile.close();
         }
         step->GetTrack()->SetTrackStatus(fStopAndKill); //kill photon
