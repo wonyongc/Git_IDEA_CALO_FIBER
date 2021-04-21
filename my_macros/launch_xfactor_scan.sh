1aa2b1c0e70e2218dafc030b10a0df806e197d08
Marco Toliman Lucchini <marco.toliman.lucchini@cern.ch>
2021-04-21 18:27:36 +0200

    fixing tails in physics events dijet matching

---
+++ my_macros/launch_xfactor_scan.sh
@@ -0,0 +1,20 @@
+./physicsJetAnalysis zjj_scan_100 50 0.445 0.39
+./physicsJetAnalysis zjj_scan_100 50 0.445 0.38
+./physicsJetAnalysis zjj_scan_100 50 0.445 0.37
+./physicsJetAnalysis zjj_scan_100 50 0.445 0.36
+./physicsJetAnalysis zjj_scan_100 50 0.445 0.35
+./physicsJetAnalysis zjj_scan_100 50 0.445 0.34
+./physicsJetAnalysis zjj_scan_100 50 0.445 0.33
+
+./physicsJetAnalysis zjj_scan_100 50 0.470 0.37
+./physicsJetAnalysis zjj_scan_100 50 0.460 0.37
+./physicsJetAnalysis zjj_scan_100 50 0.450 0.37
+./physicsJetAnalysis zjj_scan_100 50 0.445 0.37
+./physicsJetAnalysis zjj_scan_100 50 0.440 0.37
+./physicsJetAnalysis zjj_scan_100 50 0.430 0.37
+./physicsJetAnalysis zjj_scan_100 50 0.420 0.37
+
+./physicsJetAnalysis zjj_scan_100 50 0.430 0.38
+./physicsJetAnalysis zjj_scan_100 50 0.430 0.37
+./physicsJetAnalysis zjj_scan_100 50 0.430 0.36
+./physicsJetAnalysis zjj_scan_100 50 0.430 0.35
