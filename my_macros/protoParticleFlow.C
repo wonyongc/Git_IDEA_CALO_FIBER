1b46e8d62b5d5d0803cffde264097cc7c9589ee3
Marco Toliman Lucchini <marco.toliman.lucchini@cern.ch>
2021-06-02 21:57:54 +0200

    bug fix, remove of bias on raw energy cut, updated pfa pion response

--- my_macros/protoParticleFlow.C
+++ my_macros/protoParticleFlow.C
@@ -109,7 +109,7 @@ int main(int argc, char** argv)
     
   //init  
   bool SAVEPLOTS = false;  
-  bool local     = true;
+  bool local     = false;
   bool debugMode = false;
   
   TApplication* theApp;
